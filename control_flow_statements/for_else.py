a05713dbee9dd227830ae5ab6c1ca4153699680d
Nadeem-Ahmed <nadeemahmed@mirafra.com>
2021-09-29 14:25:39 +0530

    Adding pass_statement

--- control_flow_statements/for_else.py
+++ control_flow_statements/for_else.py
@@ -15,3 +15,24 @@ for num in numbers:
     break;
 else:
   print("Not Found");
+
+
+#--------------------------------------------------------------------------------------------
+#Description   : For-else loop
+#Functionality : Else will be executed only when for loop exits normally without break statement.
+#Syntax : 
+#     for num in nums:
+#       statements;
+#     else:
+#       statements;
+#--------------------------------------------------------------------------------------------
+
+
+numbers2 = [12,19,49,67,33,89];
+
+for num in numbers2:
+  if(num % 7 == 0):
+    print(num);
+    break;
+else:
+  print("Not Found");
